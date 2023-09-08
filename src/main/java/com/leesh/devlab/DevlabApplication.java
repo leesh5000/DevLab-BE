17c05ad9b6c4d1dee08065f987a9128c564d5b96
leesh5000 <leesh50000@gmail.com>
2023-09-08 23:34:36 +0900

    #12 - 프로젝트 초기화
    Spring Initializr 웹을 통해 Java + Spring 개발 환경을 설정하였습니다.
    
    Reference : https://start.spring.io/

---
+++ src/main/java/com/leesh/devlab/DevlabApplication.java
@@ -0,0 +1,13 @@
+package com.leesh.devlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlabApplication.class, args);
+	}
+
+}
