c159c3f6cd3b54dc31aac5b81fdb1b3623ee5ad3
leesh5000 <leesh50000@gmail.com>
2023-09-09 17:41:51 +0900

    #14 - 도메인 코드 개발
    - ERD를 참고하여 도메인 코드를 개발하였습니다.
    
    * 현재 도메인 영역는 특정 기술(JPA)과 강하게 결합되어 의존하고 있습니다. 이것은 변경에 대한 확장성 측면에서 좋지 못합니다. 하지만 이 프로젝트에서는 JPA 기술은 반드시 사용할 것이므로, 이 부분은 타협하여 도메인 영역 내에 특정 기술에 대한 코드를 사용하였습니다.

---
+++ src/main/java/com/leesh/devlab/domain/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.leesh.devlab.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    /* Meta Data Start */
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by", nullable = false)
+    private String modifiedBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+    /* Meta Data End */
+
+}
