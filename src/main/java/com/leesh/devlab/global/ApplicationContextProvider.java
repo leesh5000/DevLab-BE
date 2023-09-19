ce5380738b8d8c596fb42de5292b4f7b061de0bc
leesh5000 <leesh50000@gmail.com>
2023-09-19 14:48:13 +0900

    #18 - 주석 추가

--- src/main/java/com/leesh/devlab/global/ApplicationContextProvider.java
+++ src/main/java/com/leesh/devlab/global/ApplicationContextProvider.java
@@ -1,11 +1,16 @@
 package com.leesh.devlab.global;
 
+import com.leesh.devlab.domain.member.constant.OauthType;
 import lombok.Getter;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+/**
+ * 소셜 로그인 시도한 유저 요청의 경우에 알맞은 Oauth Provider를 찾기 위해 BeanName을 찾기 위해 사용
+ * Ref {@link com.leesh.devlab.external.OauthClientFactory#getService(OauthType)}
+ */
 @Getter
 @Component
 public class ApplicationContextProvider implements ApplicationContextAware {
