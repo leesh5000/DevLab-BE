eba2bbf39edadfe9db7f3c99c7f00b756239b55a
leesh5000 <leesh50000@gmail.com>
2023-09-19 14:51:35 +0900

    #18 - 사용하지 않는 메서드 제거

--- src/main/java/com/leesh/devlab/constant/TokenType.java
+++ src/main/java/com/leesh/devlab/constant/TokenType.java
@@ -16,12 +16,4 @@ public enum TokenType {
         this.expiresIn = expiresIn;
     }
 
-    public static boolean isAccessToken(String tokenType) {
-        return TokenType.ACCESS.name().equals(tokenType);
-    }
-
-    public static boolean isRefreshToken(String tokenType) {
-        return TokenType.REFRESH.name().equals(tokenType);
-    }
-
 }
