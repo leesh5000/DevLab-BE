ba166a876eb7368a5202931a5902e43509e6fdaa
leesh5000 <leesh50000@gmail.com>
2023-09-19 11:21:49 +0900

    #18 - 만료시간 변수명 수정
    ExpirationTime는 토큰이 만료되는 시간인지 토큰의 유효기간인지 불분명하기 때문에 변수명을 ValidTime으로 수정하였습니다.

--- src/main/java/com/leesh/devlab/global/jwt/JwtService.java
+++ src/main/java/com/leesh/devlab/global/jwt/JwtService.java
@@ -1,32 +1,37 @@
 package com.leesh.devlab.global.jwt;
 
 import com.leesh.devlab.domain.member.Member;
+import com.leesh.devlab.global.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
+import java.util.Date;
 
 @Slf4j
 @Service
 public class JwtService implements TokenService {
 
-    private final Long accessTokenExpirationTime;
-    private final Long refreshTokenExpirationTime;
+    private final Long accessTokenValidTime;
+    private final Long refreshTokenValidTime;
     private final String tokenSecret;
 
-    public JwtService(@Value("${token.access-token-expiration-time}") Long accessTokenExpirationTime,
-                      @Value("${token.refresh-token-expiration-time}") Long refreshTokenExpirationTime,
+    public JwtService(@Value("${token.access-token-expiration-time}") Long accessTokenValidTime,
+                      @Value("${token.refresh-token-expiration-time}") Long refreshTokenValidTime,
                       @Value("${token.secret}") String tokenSecret) {
 
-        this.accessTokenExpirationTime = accessTokenExpirationTime;
-        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
+        this.accessTokenValidTime = accessTokenValidTime;
+        this.refreshTokenValidTime = refreshTokenValidTime;
         this.tokenSecret = tokenSecret;
 
     }
 
     @Override
-    public MemberInfo extractMemberInfo(String accessToken) throws AuthenticationException {
+    public MemberInfo extractMemberInfo(Jwt jwt) throws BusinessException {
+
+        // Access Token 이 아니면, 예외 던지기
+
         return null;
     }
 
@@ -42,6 +47,9 @@ public class JwtService implements TokenService {
 
     @Override
     public Jwt.AccessToken createAccessToken(Member member) {
+
+        Date expirationTime = new Date(System.currentTimeMillis() + accessTokenValidTime);
+
         return null;
     }
 
