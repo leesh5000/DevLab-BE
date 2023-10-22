4439dccae9369acb1371310f2ff633efa31e37d4
leesh5000 <leesh50000@gmail.com>
2023-10-23 03:22:10 +0900

    #48 - 보안코드 생성 로직 수정

--- src/main/java/com/leesh/devlab/external/OauthAttributes.java
+++ src/main/java/com/leesh/devlab/external/OauthAttributes.java
@@ -3,20 +3,14 @@ package com.leesh.devlab.external;
 import com.leesh.devlab.domain.member.Member;
 import com.leesh.devlab.domain.member.OauthType;
 
-import java.util.UUID;
-
 public interface OauthAttributes {
 
     String getId();
 
     OauthType getOauthType();
 
-    default String getNickname() {
-        return getOauthType().toString().charAt(0) + UUID.randomUUID().toString().split("-")[0];
-    };
-
     default Member toEntity() {
-        return Member.of(getOauthType(), getId(), getNickname());
+        return Member.createMember(this);
     }
 
 }
